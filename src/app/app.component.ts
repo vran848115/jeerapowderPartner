ca36d7df0f5a1f107586289cddea58e5f1bc2155
Shekhar Suman <shekhar_suman@outlook.com>
2018-12-25 03:03:07 +0530

    basic changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,14 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import { ChefLoginPage } from '../pages/chef-login/chef-login';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  // rootPage:any = TabsPage;
+  rootPage:any = ChefLoginPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
--- src/pages/chef-login/chef-login.ts
+++ src/pages/chef-login/chef-login.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TabsPage } from '../tabs/tabs';
 
-/**
- * Generated class for the ChefLoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -20,6 +15,10 @@ export class ChefLoginPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ChefLoginPage');
+  }
+
+  navigateToChefHome(){
+    this.navCtrl.setRoot(TabsPage);
   }
 
 }
